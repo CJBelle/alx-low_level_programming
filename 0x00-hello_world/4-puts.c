1c33c4859c461396631002e372265d50ab284898
CJBelle <chernaybalfour@gmail.com>
2023-03-20 14:41:23 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-   puts("Programming is like building a multilingual puzzle");
-   return(0);
+
+int puts("Programming is like building a multilingual puzzle");
+
+return (0);
+
 }
